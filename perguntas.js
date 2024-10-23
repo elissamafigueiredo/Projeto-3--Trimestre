4cfe99ebe98a4cc05fbb379626861f5ae25db7b7
elissamafigueiredo <163293180+elissamafigueiredo@users.noreply.github.com>
2024-10-23 08:42:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Diabetes',
+    'O que Diabetes causa?',
+    'O diabetes pode causar o aumento da glicemia e as altas taxas podem levar a complicações no coração, nas artérias, nos olhos, nos rins e nos nervos. Em casos mais graves, o diabetes pode levar à morte.'
+)
+
+criaCartao(
+    'Diabetes',
+    'Qual é o normal do Diabetes?',
+    'O estado de normalidade da glicemia em jejum é de 70 mg/dl a 100 mg/ld. Uma pessoa é classificada como pré-diabética ao medir a sua glicemia em jejum e atingir entre 100 e 125 mg/dl. Já aqueles que atingem a partir de 126 mg/dl são considerados diabéticos.'
+)
+
+criaCartao(
+    'Diabetes',
+    'Quais são os tipos de Diabetes?',
+    'Pré-diabetes; Diabetes tipo 1; Diabetes tipo 2; Diabetes Gestacional'
+)
+
+criaCartao(
+    ' Diabetes',
+    ' O que é Diabete Melittus?',
+    ' Diabetes Mellitus (DM) é uma síndrome metabólica de origem múltipla, decorrente da falta de insulina e/ou da incapacidade e/ou falta de insulina exercer adequadamente seus efeitos, caracterizando altas taxa de açúcar no sangue ( hiperglicemia) de forma permanente.'
+)
+
+criaCartao(
+    ' Diabetes',
+    ' O que faz aumentar a Diabetes?',
+    'Exagerar no consumo de bebidas açucaradas. Refrigerantes, sucos, chás e outras bebidas industrializadas estão repletas de açúcar, como a frutose, que leva ao ganho de peso e ao aumento da produção de insulina no organismo.'
+)
+
+criaCartao(
+    ' Diabetes',
+    'Como diagnosticar Diabetes?',
+    'O diagnóstico de diabetes é feito com base nos resultados de um exame de sangue conhecido como glicemia de jejum, esse exame é geralmente solicitado pelo clínico geral em check-ups anuais, juntamente com outros exames, como hemoglobina, creatinina, enzimas hepáticas, ácido úrico, colesterol, dentre outros.'
+)
+
+criaCartao(
+    ' Diabetes',
+    'Quando a Diabetes é perigosa?',
+    'Quando os sintomas, como fome excessiva, emagrecimento, cansaço, fraqueza, sede e diurese, são ignorados e o tratamento não é feito o quadro pode evoluir para um estágio perigoso como desidratação severa, dificuldades respiratórias, vômitos e até o coma.'
+)
+
+criaCartao(
+    ' Diabetes',
+    'É possível reverter Diabetes?',
+    'Inclusive, é praticamente impossível prever se a evolução vai, de fato, ocorrer. A boa notícia é que aproximadamente 2/3 dos pacientes conseguem reverter o quadro de intolerância à glicose para uma normal tolerância à glicose, ou seja, antes que a condição progrida efetivamente para o diabetes.'
+)
+
+criaCartao(
+    ' Diabetes',
+    'Quem possui Diabetes tem vida normal?',
+    'Muitas pessoas imaginam que viverão uma vida de privações e têm dificuldade em aceitar a condição. Mas não precisa ser assim: com os devidos cuidados e seguindo o tratamento adequado, é perfeitamente possível ter uma boa qualidade de vida.'
+)
+
